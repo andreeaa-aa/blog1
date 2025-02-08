666b3d1da9f928ac6354971478858f98faa8812d
andreeaa-aa <cretu.anndreea@gmail.com>
2025-02-08 21:53:00 +0200

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,9 @@
+function openArticles(){
+    window.location.href = "blog.html";
+}
+function openContact(){
+    window.location.href = "contact.html";
+}
+function openReviews(){
+    window.location.href = "reviews.html";
+}
